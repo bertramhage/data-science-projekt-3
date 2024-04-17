598cd3b2c22ca52efd8a37525473e4676a729296
Jacob Brams <jacobbrams@brams-mbk-pro.students.clients.local>
2024-04-17 14:27:10 +0200

    [commit besked]

--- CARS24.py
+++ CARS24.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 #load data
-cars24 = pd.read_csv('/Users/jacobbrams/Library/Mobile Documents/com~apple~CloudDocs/UNI - 4.sem DTU/Data and Data Science/Projekt 3/Part II/cars_24_combined.csv')
+cars24 = pd.read_csv('/Users/jacobbrams/Library/Mobile Documents/com~apple~CloudDocs/UNI - 4.sem DTU/Data and Data Science/Projekt 3/data-science-projekt-3/cars_24_combined.csv')
 
 #show data
 cars24.head()
